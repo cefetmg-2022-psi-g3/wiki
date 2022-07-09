3d55e88793c0224bd8a440f13d45fc5d35a1e468
PedroYagi <49000417+PedroYagi@users.noreply.github.com>
2022-07-09 19:52:08 -0300

    Add files via upload

---
+++ PovoarTabela.sql
@@ -0,0 +1,35 @@
+INSERT INTO cardapioCampus1 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Bife de boi grelhado','Omelete','Proteína de Soja Xadrez','Farofa brasileira','Arroz branco','Feijão carioca','Mista de folhas','Rabanete','Beterraba cozida','Tabule de frutas','Melão','01/07/2022');
+
+INSERT INTO cardapioCampus1 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Fricasse de frango','Fricasse de proteína de soja','Ratatoulle','Batata Palha','Arroz branco','Feijão carioca','Alface crespa','Pepino com cheiro verde','Beterraba ralada','Soja em graos c/ azeitona','Maça','04/07/2022');
+
+INSERT INTO cardapioCampus1 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Almondega ao sugo','Ovos mexidos','Feijão branco c/ legumes','Polenta','Arroz com cenoura','Feijão carioca','Almeirão com laranja','Rabanete ralado com milho','Abobrinha cozida','Couve c/ tomate e cebola','Banana','05/07/2022');
+
+INSERT INTO cardapioCampus1 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Bife de pernil ao molho barbacue','Proteína de soja c/ azeitonas','Hamburger de feijão preto','Acelga refogada','Arroz branco','Tutu de feijão','Mix de folhas','Nabo c/ cheiro verde','Chuchu cozido','Grão de bico ao vinagrete','Laranja','06/07/2022');
+
+INSERT INTO cardapioCampus1 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Iscas bovinas ao molho ferrugem','Lentilha de legumes','Tabule c/ hortela','Cará refogada','Arroz integral','Feijão carioca','Chicória','Mix de repolho','Abóbora cozida','Tomate meia lua com manjericão','Tangerina','07/07/2022');
+
+INSERT INTO cardapioCampus1 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Iscas de frango acebolada','Bolinho de arroz','Grão de bico acebolado','Farofa de Bacon','Arroz branco','Feijão carioca','Alface roxa','Pepino em rodela c/ gergelim','Batata doce com ervas finas','Cenoura colorida','Melão','08/07/2022');
+
+INSERT INTO cardapioCampus2 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Bife de boi grelhado','Omelete','Proteína de Soja Xadrez','Farofa brasileira','Arroz branco','Feijão carioca','Mista de folhas','Rabanete','Beterraba cozida','Tabule de frutas','Melão','01/07/2022');
+
+INSERT INTO cardapioCampus2 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Fricasse de frango','Fricasse de proteína de soja','Ratatoulle','Batata Palha','Arroz branco','Feijão carioca','Alface crespa','Pepino com cheiro verde','Beterraba ralada','Soja em graos c/ azeitona','Maça','04/07/2022');
+
+INSERT INTO cardapioCampus2 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Almondega ao sugo','Ovos mexidos','Feijão branco c/ legumes','Polenta','Arroz com cenoura','Feijão carioca','Almeirão com laranja','Rabanete ralado com milho','Abobrinha cozida','Couve c/ tomate e cebola','Banana','05/07/2022');
+
+INSERT INTO cardapioCampus2 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Bife de pernil ao molho barbacue','Proteína de soja c/ azeitonas','Hamburger de feijão preto','Acelga refogada','Arroz branco','Tutu de feijão','Mix de folhas','Nabo c/ cheiro verde','Chuchu cozido','Grão de bico ao vinagrete','Laranja','06/07/2022');
+
+INSERT INTO cardapioCampus2 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Iscas bovinas ao molho ferrugem','Lentilha de legumes','Tabule c/ hortela','Cará refogada','Arroz integral','Feijão carioca','Chicória','Mix de repolho','Abóbora cozida','Tomate meia lua com manjericão','Tangerina','07/07/2022');
+
+INSERT INTO cardapioCampus2 (prato_proteico,vegetariano,vegano,guarnicao,arroz,feijao,salada1,salada2,salada3,salada4,sobremesa,dia)
+VALUES ('Iscas de frango acebolada','Bolinho de arroz','Grão de bico acebolado','Farofa de Bacon','Arroz branco','Feijão carioca','Alface roxa','Pepino em rodela c/ gergelim','Batata doce com ervas finas','Cenoura colorida','Melão','08/07/2022');
