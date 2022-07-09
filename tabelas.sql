f5eb7451273d3ae11ce5211d87504530cc0269bc
PedroYagi <49000417+PedroYagi@users.noreply.github.com>
2022-07-09 19:52:56 -0300

    Add files via upload

---
+++ tabelas.sql
@@ -0,0 +1,29 @@
+CREATE TABLE cardapioCampus1(
+	prato_proteico varchar(50) not null,
+	vegetariano varchar (50) not null,
+	vegano varchar (50) not null,
+	guarnicao varchar (50) not null,
+	arroz varchar (50) not null,
+	feijao varchar (50) not null,
+	salada1 varchar (50) not null,
+	salada2 varchar (50) not null,
+	salada3 varchar (50) not null,
+	salada4 varchar (50) not null,
+	sobremesa varchar (50) not null,
+	dia date not null primary key
+);
+
+CREATE TABLE cardapioCampus2(
+	prato_proteico varchar(50) not null,
+	vegetariano varchar (50) not null,
+	vegano varchar (50) not null,
+	guarnicao varchar (50) not null,
+	arroz varchar (50) not null,
+	feijao varchar (50) not null,
+	salada1 varchar (50) not null,
+	salada2 varchar (50) not null,
+	salada3 varchar (50) not null,
+	salada4 varchar (50) not null,
+	sobremesa varchar (50) not null,
+	dia date not null primary key	
+);
